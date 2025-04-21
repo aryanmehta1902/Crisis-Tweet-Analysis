34ff3104c58426937d90c5082cacc70f1314f68c
Viraaj Veeramachaneni <veeramachaneni.v2@gmail.com>
2025-04-21 13:52:27 -0500

    Update sidebar links and add Statistics page

---
+++ src/components/Statistics.js
@@ -0,0 +1,12 @@
+// src/components/Statistics.js
+import React from 'react';
+import './Statistics.css';    
+
+export default function Statistics() {
+  return (
+    <div className="statistics-container">  {/* ← and this matches your CSS selector */}
+      <h1>Statistics</h1>
+      <p>Here you can display charts, numbers, summaries, etc.</p>
+    </div>
+  );
+}
